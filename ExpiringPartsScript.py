edb8b639141097625c9708afdf528f628e29d75d
Jesse-McNicoll <jesse.t.mcnicoll@gmail.com>
2017-08-23 09:39:37 -0500

    The bare beginnings.  No real functionality.

---
+++ ExpiringPartsScript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python27
+
+# Title: ExpiringPartsScript.py
+# Author: Jesse McNicoll
+# Date Created: 8/23/2017
+# Description:
+#   This script will query the erpsql server and run a query to find expiring vendor costs.
+#   If any vendors are expiring within the next six weeks, the script will automatically send an
+#   email to their point of contact asking for new prices.
+#
+
+print "This is a script"
