09c53d0749c35341fbfb8ab93a4fecc82e88a427
Jesse-McNicoll <jesse.t.mcnicoll@gmail.com>
2017-08-31 17:28:31 -0500

    some successful querying and comments added

--- ExpiringPartsScript.py
+++ ExpiringPartsScript.py
@@ -19,7 +19,7 @@ database = 'EpicorTest10'
 # the correct database.
 connection_string = 'Driver={SQL Server};Server=ERPSQL;Database=%s;Trusted_Connection=yes;' % (database)
 
-#Create a
+#Create a string for the query that can be easily updated with a new database, allowing mutability of code.
 query_string = '''SELECT     T1.VendorID, T1.ExpirationDate
 FROM         (SELECT     %(db)s.dbo.Vendor.VendorID, MAX(%(db)s.Erp.VendPart.ExpirationDate) AS ExpirationDate
               FROM          %(db)s.Erp.VendPart INNER JOIN
@@ -27,19 +27,25 @@ FROM         (SELECT     %(db)s.dbo.Vendor.VendorID, MAX(%(db)s.Erp.VendPart.Exp
               GROUP BY %(db)s.dbo.Vendor.VendorID) AS T1
 WHERE     (ExpirationDate <= DATEADD(month, 1, GETDATE()))''' % {'db': database}
 
-print query_string
-#connection = pyodbc.connect(conn_str)
+#Now, use the connection string as the input to the pyodbc.connect() method to
+# to form the database connection.
 connection = pyodbc.connect(connection_string)
+#Instantiate a cursor to the connection to allow a query to be performed.
 cursor = connection.cursor()
-
+#Execute the query_string with cursor.execute() method to get the expiring vendor data.
 cursor.execute (query_string)
+#Obtain all the results to allow them to be parsed through.
+results = cursor.fetchall()
 
-results = cursor.fetchone()
-
+#Output a python desktop notification to inform the operator of the incoming
+# expiration date and confirm whether they want to send an email to the vendor.
 print "The expired vendor table:"
+for row in results:
+    print ("Vendor: " + row[0] + "  ExpirationDate: " + row[1])
 
-while results:
-    print ("Vendor: " + results[0] + "ExpirationDate" + results[1])
+for row in results:
+    print ("Vendor: " + row[0] + "  ExpirationDate: " + row[1])
 
-results = cursor.fetchone()
+
+
 connection.close()
